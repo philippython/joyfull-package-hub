e12a30846ac05f39dbff1cd2dd9485c883a60365
philippython <odulajaphilip@gmail.com>
2026-06-15 21:15:09 +0100

    Updated server.mjs

--- netlify/functions/server.mjs
+++ netlify/functions/server.mjs
@@ -1,7 +1,14 @@
-import handler from "./server-bundle.js";
+import { createRequire } from "module";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const handler = await import(join(__dirname, "server-bundle.js"));
 
 export default async (req, context) => {
-  return handler.fetch(req, process.env, context);
+  return handler.default.fetch(req, process.env, context);
 };
 
 export const config = {
