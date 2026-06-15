16593ae3ae3c6118dcda88e3f317743c856176b3
philippython <odulajaphilip@gmail.com>
2026-06-15 15:24:40 +0100

    Deploy assets fixes

--- netlify/functions/server.mjs
+++ netlify/functions/server.mjs
@@ -1,9 +1,10 @@
 import handler from "../../dist/server/server.js";
 
 export default async (req, context) => {
-  return handler.fetch(req, {}, context);
+  return handler.fetch(req, process.env, context);
 };
 
 export const config = {
   path: "/*",
+  excludedPath: ["/assets/*"],
 };
